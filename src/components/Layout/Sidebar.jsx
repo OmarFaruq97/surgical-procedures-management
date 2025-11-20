1d5de385d63e652dd749907ae4661b8ec71ad211
Omar Faruq <omarfaruq9723@gmail.com>
2025-11-20 09:43:52 +0600

    init

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -9,8 +9,8 @@ const Sidebar = () => {
           <li><Link to="/dashboard" className="block py-2 px-4 hover:bg-gray-700 rounded">Dashboard</Link></li>
           <li><Link to="/procedures" className="block py-2 px-4 hover:bg-gray-700 rounded">Surgical Procedures</Link></li>
           <li><Link to="/reports" className="block py-2 px-4 hover:bg-gray-700 rounded">Reports</Link></li>
-          <li><Link to="/" className="block py-2 px-4 hover:bg-gray-700 rounded">Test</Link></li>
-          <li><Link to="/" className="block py-2 px-4 hover:bg-gray-700 rounded">Test2</Link></li>
+          <li><Link to="/notfound" className="block py-2 px-4 hover:bg-gray-700 rounded">Test</Link></li>
+          <li><Link to="notfound" className="block py-2 px-4 hover:bg-gray-700 rounded">Test2</Link></li>
           <li><Link to="/notfound" className="block py-2 px-4 hover:bg-gray-700 rounded">NotFoundTest3</Link></li>
         </ul>
       </nav>
